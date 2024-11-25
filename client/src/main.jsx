d435db1289397c4dd15dfb1502cdde51ae551f2b
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-11-25 14:03:08 -0300

    6

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider} from 'react-redux'
 import { store } from './redux/store/index.js'
 import {BrowserRouter} from 'react-router-dom'
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 import './index.css'
 import App from './App.jsx'
@@ -20,4 +20,4 @@ createRoot(document.getElementById('root')).render(
 )
 
 
-reportWebVitals()
+// reportWebVitals()
