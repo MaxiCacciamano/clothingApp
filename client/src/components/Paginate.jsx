ff524504b2e745175e36b80d15c57beec40ce59e
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-11-25 12:23:18 -0300

    3

---
+++ client/src/components/Paginate.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Paginate = () => {
+  return (
+    <div>Paginate</div>
+  )
+}
---
+++ client/src/components/SearchName.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SearchName = () => {
+  return (
+    <div>SearchName</div>
+  )
+}
