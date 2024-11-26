7b9fe044043a2c024b2f134d31a8058e8002edf9
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-11-26 14:27:07 -0300

    77

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -9,12 +9,14 @@ export default function rootReducer(state = initialState, action){
     console.log(action.payload,"aca esta el valor")
     const womenCategory = clothing.catalog.clothing.categories.find(category => category.name === "Women");
     const categoryName = womenCategory.categories.find(category => category.categories)
-    const nameProd = categoryName.categories.find(prodNmae => prodNmae.categories)
+    const perCategory = categoryName.categories.filter(prodNmae => prodNmae.categories)
+    const nameProd = perCategory.map(e=>e.categories.amount)
     // const filterCategory = categoryName.categories.filter(e => e.name)
     // const name = filterCategory.categories.map(e=>e.categories)
     console.log(womenCategory,"women")
     console.log(categoryName,"categories")
-    console.log(nameProd, "name Prdouct")
+    console.log(perCategory, "per Prdouct")
+    console.log(nameProd, "name prod")
     switch(action.type){
         case 'GET_MEN':
             return{
