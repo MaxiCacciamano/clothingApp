d764b5ed7bf738e26ab74d755a67ba9deeb55182
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-12-16 11:55:51 -0300

    5

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,12 +5,19 @@ import style from '../design/nav.module.css'
 import { SearchName } from './SearchName'
 
 import log from '../../public/IMG/Group 53.png'
+import favoritos from '../../public/IMG/heart.png'
+import cart from '../../public/IMG/Cart.png'
+import login from '../../public/IMG/login.png'
 
 export const Nav = () => {
   return (
     <div className={style.contain}>
      <div className={style.Frame556}>
-        <h2>_</h2>
+    <div className={style.line}>
+    <hr style={{border:'0',height:'2px',backgroundColor:'#333', margin:' 0', width:'30px', marginBottom: '8px'}}/>
+    <hr style={{border:'0',height:'2px',backgroundColor:'#333', margin:' 0', width:'20px', marginBottom: '8px'}}/>
+    <hr style={{border:'0',height:'2px',backgroundColor:'#333', margin:' 0', width:'10px',}}/>
+    </div>
      <div className={style.menu}>
        <ul>
          <li>Home</li>
@@ -23,10 +30,23 @@ export const Nav = () => {
       <img src={log} />
      </div>
 
-     <div className={style.Frame552}>
-      <div>
-       <SearchName/>
-      </div>
+
+     <div className={style.Frame552} >
+     <div className={style.favorito}>
+       <img src= {favoritos} />
+     </div>
+       <div className={style.cart}>
+        <div className={style.textoCart}>
+         <p>Cart</p>
+        </div>
+        <div className={style.logoCarrito}>
+         <img src={cart} />
+        </div>
+       </div>
+       <div className={style.login}>
+       <img src={login} />
+       </div>
+       {/* <SearchName/> */}
      </div>
     </div>
   )
