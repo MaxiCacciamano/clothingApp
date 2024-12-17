9980019dfae6bedb839eca5747c646ff2432b631
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-12-17 09:33:12 -0300

    5

---
+++ client/src/components/Hamburgesa.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+import style from '../design/hamburgesa.module.css'
+
+export const Hamburgesa = () => {
+      const [isOpen, setIsOpen] = useState(false)
+    
+      const toggleMenu = ( ) => {
+        setIsOpen(!isOpen)
+      }
+  return (
+    <div>
+
+             <div className={`${style.menuToggle} ${isOpen? style.open : ''}`} 
+             onClick={toggleMenu}>
+             {/*Menu hamburgesa*/ }
+            <div className={style.line}>
+            <hr className={style.bar} style={{ border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'30px', marginBottom: '6px'}}/>
+            <hr className={style.bar} style={{ border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'20px', marginBottom: '6px'}}/>
+            <hr className={style.bar} style={{ border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'10px',}}/>
+            </div>
+             {/* Menú desplegable */}
+             <div>
+              <nav className={`${style.nav} ${isOpen? style.show:''}`}>
+                <ul>
+                  <li><a href="#">Men</a></li>
+                  <li><a href="#">Women</a></li>
+                  <li><a href="#"></a>Kids</li>
+                </ul>
+              </nav>
+               {/* <SearchName/> */}
+             </div>
+             </div>
+    </div>
+  )
+}
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,37 +8,13 @@ import log from '../../public/IMG/Group 53.png'
 import favoritos from '../../public/IMG/heart.png'
 import cart from '../../public/IMG/Cart.png'
 import login from '../../public/IMG/login.png'
+import { Hamburgesa } from './Hamburgesa'
 
 export const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggleMenu = ( ) => {
-    setIsOpen(!isOpen)
-  }
   return (
     <div className={style.contain}>
      <div className={style.Frame556}>
-     {/*Menu hamburgesa*/ }
-     <div className={`${style.menuToggle} ${isOpen? style.open : ''}`} 
-     onClick={toggleMenu}>
-    <div className={style.line}>
-    <hr className={style.bar} style={{border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'30px', marginBottom: '6px'}}/>
-    <hr className={style.bar} style={{border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'20px', marginBottom: '6px'}}/>
-    <hr className={style.bar} style={{border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'10px',}}/>
-    </div>
-     </div>
-
-     {/* Menú desplegable */}
-     <div>
-      <nav className={`${style.nav} ${isOpen? style.show:''}`}>
-        <ul>
-          <li><a href="#">Men</a></li>
-          <li><a href="#">Women</a></li>
-          <li><a href="#"></a>Kids</li>
-        </ul>
-      </nav>
-       {/* <SearchName/> */}
-     </div>
+     <Hamburgesa/>
      <div className={style.menu}>
        <ul>
          <li><p>Home</p></li>
@@ -50,8 +26,6 @@ export const Nav = () => {
      <div className={style.log} >
       <img src={log} />
      </div>
-
-
      <div className={style.Frame552} >
      <div className={style.favorito}>
        <img src= {favoritos} />
