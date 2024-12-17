76ebe9ff09de00126d4b1a3707e5a836a04ec39a
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-12-17 08:55:56 -0300

    4

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Filters } from './Filters'
 
 import style from '../design/nav.module.css'
@@ -10,14 +10,35 @@ import cart from '../../public/IMG/Cart.png'
 import login from '../../public/IMG/login.png'
 
 export const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = ( ) => {
+    setIsOpen(!isOpen)
+  }
   return (
     <div className={style.contain}>
      <div className={style.Frame556}>
+     {/*Menu hamburgesa*/ }
+     <div className={`${style.menuToggle} ${isOpen? style.open : ''}`} 
+     onClick={toggleMenu}>
     <div className={style.line}>
-    <hr style={{border:'0',height:'2px',backgroundColor:'#333', margin:' 0', width:'30px', marginBottom: '8px'}}/>
-    <hr style={{border:'0',height:'2px',backgroundColor:'#333', margin:' 0', width:'20px', marginBottom: '8px'}}/>
-    <hr style={{border:'0',height:'2px',backgroundColor:'#333', margin:' 0', width:'10px',}}/>
+    <hr className={style.bar} style={{border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'30px', marginBottom: '6px'}}/>
+    <hr className={style.bar} style={{border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'20px', marginBottom: '6px'}}/>
+    <hr className={style.bar} style={{border:'0',height:'1.5px',backgroundColor:'#333', margin:' 0', width:'10px',}}/>
     </div>
+     </div>
+
+     {/* Menú desplegable */}
+     <div>
+      <nav className={`${style.nav} ${isOpen? style.show:''}`}>
+        <ul>
+          <li><a href="#">Men</a></li>
+          <li><a href="#">Women</a></li>
+          <li><a href="#"></a>Kids</li>
+        </ul>
+      </nav>
+       {/* <SearchName/> */}
+     </div>
      <div className={style.menu}>
        <ul>
          <li><p>Home</p></li>
@@ -46,7 +67,9 @@ export const Nav = () => {
        <div className={style.login}>
        <img src={login} />
        </div>
-       {/* <SearchName/> */}
+     </div>
+     <div>
+
      </div>
     </div>
   )
