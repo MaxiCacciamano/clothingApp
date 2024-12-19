dbd210d0714e3baf0f9d799cb9724b6b3381938b
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-12-19 12:11:59 -0300

    4

---
+++ client/src/components/Newcollection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Newcollection = () => {
+  return (
+    <div>
+    <div>
+    <h1>New collection</h1>
+    <p>Summer</p>
+    <p>2024</p>
+    </div>}
+    
+    </div>
+  )
+}
