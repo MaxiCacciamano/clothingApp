4cd69dbd2cd6e139462ed36ce0fc19ab19c9e2a1
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-12-19 12:47:39 -0300

    5

--- client/src/components/Newcollection.jsx
+++ client/src/components/Newcollection.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from 'react-slick'; // Importamos el componente Slider de react-slick
 
 import arrow from '../../public/IMG/Arrow.png'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAccesorios } from '../redux/action';
 
 export const Newcollection = () => {
+  const dispatch = useDispatch()
+      const accessories = useSelector(state=>state.accessories)
     // Configuración básica para el carrusel
     const settings = {
       dots: true, // Mostrar puntos de navegación
@@ -12,6 +16,10 @@ export const Newcollection = () => {
       slidesToShow: 1, // Cuántas imágenes se mostrarán a la vez
       slidesToScroll: 1, // Cuántas imágenes se desplazan por vez
     };
+
+    useEffect(()=>{
+      dispatch(getAccesorios())
+    })
   return (
     <div>
     <div>
@@ -21,8 +29,13 @@ export const Newcollection = () => {
     <div>
      <h2>Carrousel de imagenes</h2>
      <Slider {...settings}>
+     {
+      accessories.map((e, index)=>(
+        <img key={`${e.name}-${index}`} src= {e.image} />
+      ))
+     }
      <div>
-      <img src='https://via.placeholder.com/600x400?text=Imagen+1' alt='Imagen 1'/>
+      <img src='../../clothing.json' alt='Imagen 1'/>
      </div>
      <div>
       <img src='https://via.placeholder.com/600x400?text=Imagen+2' alt='Imagen 2'/>
