87616bfef3615c6d0a3f954be0f8138e31b35e95
MaxiCacciamano <maximilianocacciamanomail.com>
2024-12-23 16:27:25 -0300

    3

--- client/src/components/Newcollection.jsx
+++ client/src/components/Newcollection.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import Slider from 'react-slick'; // Importamos el componente Slider de react-slick
 
 import arrow from '../../public/IMG/Arrow.png'
+import carrusel from '../../public/IMG/Vector.png'
+
 import { useDispatch, useSelector } from 'react-redux';
 import { getAccesorios } from '../redux/action';
 
@@ -10,6 +12,32 @@ import style from '../design/getall.module.css'
 export const Newcollection = () => {
   const dispatch = useDispatch()
       const accessories = useSelector(state=>state.accessories)
+
+
+          //Componente personalizado para la flecha
+    const CustomPrevArrow= (props) => {
+      const { className, style, onClick } = props;
+      return(
+        <div
+        // style={{ ...style, display: "block", left: "140px", zIndex: 10 }}
+        onClick={onClick}
+        >
+          <img src={carrusel}/>
+        </div>
+      )
+    }
+
+    const CustomNextArrow = (props)=>{
+      const { className, style, onClick } = props;
+      return(
+        <div
+        // style={{ ...style, display: "block", right: "-40px", zIndex: 10 }}
+        onClick={onClick}
+        >
+          <img src={carrusel}/>
+        </div>
+      )
+    }
     // Configuración básica para el carrusel
     const settings = {
       dots: true, // Mostrar puntos de navegación
@@ -17,7 +45,11 @@ export const Newcollection = () => {
       speed: 500, // Velocidad de transición en milisegundos
       slidesToShow: 2, // Cuántas imágenes se mostrarán a la vez
       slidesToScroll: 1, // Cuántas imágenes se desplazan por vez
+      arrow:true,
+      prevArrow:<CustomPrevArrow/>,
+      nextArrow:<CustomNextArrow/>
     };
+
 
     useEffect(()=>{
       dispatch(getAccesorios())
@@ -28,9 +60,9 @@ export const Newcollection = () => {
      <div className={style.presentacion}>
       <h1>New<br/> collection</h1>
       <p>Summer<br/> 2024</p>  
+      <button>Go To Shop <img src={arrow}/></button>
      </div>
         <div className={style.carrusel}>
-      <h2>Carrousel de imagenes</h2>
      <Slider {...settings}>
      {
       accessories.map((e, index)=>(
