1734173d2bcbaedc171c4d59f00a42a6ea6885ed
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-12-27 14:21:16 -0300

    7l

--- client/src/components/Filtercategory.jsx
+++ client/src/components/Filtercategory.jsx
@@ -6,23 +6,34 @@ import style from '../design/filterCat.module.css'
 
 export const Filtercategory = () => {
     const dispatch = useDispatch()
-    function handleFilterCategory(e){
-        e.preventDefault();
-        const selectedCategory = e.target.value
+    function handleFilterCategory(category){
+        // e.preventDefault();
+        // const selectedCategory = e.target.value
         // console.log(selectedCategory, "e.target.value")
-        dispatch(filterCategories(e.target.value))
+        dispatch(filterCategories(category))
     }
+
+    const categories = ["ALL", "Dresses", "Tops", "Pants", "Shorts"]
   return (
     <div>
       <div style={{display:'flex', flexDirection:'row'}} className={style.filterClothing}>
         {/* <h2>Category</h2> */}
-        <ul name='category' onChange={e => handleFilterCategory(e)}>
-            <li><option value="All">All</option></li>
+        <ul>
+        {
+          categories.map((category, index)=>(
+            <li key={index}
+            onClick={()=>handleFilterCategory(category)}
+            >
+            {category}
+            </li>
+          ))
+        }
+            {/* <li><option value="All">All</option></li>
             <li><option value="Dresses">Dresses</option></li>
             <li><option value="Tops">Tops</option></li>
-            <li><option value="Pants">Pants</option></li>
+            <li><option value="Pants">Pants</option></li> */}
             {/* <option value="Skirts">Skirts</option> */}
-            <li><option value="Shorts">Shorts</option></li>
+            {/* <li><option value="Shorts">Shorts</option></li> */}
         </ul>
       </div>
         </div>
